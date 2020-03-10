60d2ba075b2e1bbe93b037ce5498ed5249277c49
tonochi.tsubasa <tonochi.tsubasa@ACRO-NOTE-NO47.inc.acrovision>
2020-03-10 16:30:29 +0900

    secand

--- dotto/src/dotto/MyApp.java
+++ dotto/src/dotto/MyApp.java
@@ -1,14 +1,24 @@
 package dotto;
 
-public class Human{
-	private String name;
-	private int age;
-}
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-public String getName() {
-	return name;
-}
+public class MyApp {
 
-public class Student extends Human{
+  public static void main(String[] args) {
+    LocalDateTime d = LocalDateTime.now();
+    // LocalDateTime d = LocalDateTime.of(2016, 1, 1, 10, 10, 10);
+    // LocalDateTime d = LocalDateTime.parse("2016-01-01T10:10:10");
+
+    System.out.println(d.getYear());
+    System.out.println(d.getMonth());
+    System.out.println(d.getMonth().getValue());
+
+    System.out.println(d.plusMonths(2).minusDays(3));
+
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy!MM!dd!");
+    System.out.println(d.format(dtf));
+
+  }
 
 }
